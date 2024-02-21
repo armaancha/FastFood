fb26fd9fc265aabe993414c0a4bb3c2fb3d70ab5
armaancha <145879027+armaancha@users.noreply.github.com>
2024-02-20 21:40:57 -0800

    Create Checkout.js

---
+++ src/Checkout.js
@@ -0,0 +1,51 @@
+import "./styles.css";
+
+export default function Checkout(props) {
+  function getTotal() {
+    let total = 0;
+    for(let i=0; i<props.order.length; i++) {
+      total+=(props.order[i].price*props.order[i].count);
+    }
+    return total;
+  }
+
+  return(
+    <div className="contentPage">
+      <h1>Checkout</h1>
+      <div id="orderItems">
+        {props.order.map((item) => {
+          return(
+           <OrderItem item={item} removeItem={props.removeItem} addCount={props.addCount} minusCount={props.minusCount}/>
+          )
+        })}
+      </div>
+      { props.order.length>0 ? 
+        (
+          <div id="totalDiv">
+            <h2>Subtotal: ${getTotal().toFixed(2)}</h2>
+          </div>
+        ) : 
+        <h2>Nothing in cart :(</h2>
+      }
+
+    </div>
+  )
+}
+
+function OrderItem(props) {
+  return (
+    <div className="item">
+      <img src={props.item.image} width="170" />
+      <div className="itemDetails">
+        <h2>{props.item.name}</h2>
+        <h2>${props.item.price}</h2>
+        <button onClick={() => {props.removeItem(props.item)}}>Remove</button>
+      </div>
+      <div className="countDiv">
+        <button onClick={() => {props.minusCount(props.item)}}>-</button>
+        <h2>x{props.item.count}</h2>
+        <button onClick={() => {props.addCount(props.item)}}>+</button>
+      </div>
+    </div>
+  )
+}
