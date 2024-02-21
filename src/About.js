062bb40f2c34dd70771347d3915e601dd8bb374b
armaancha <145879027+armaancha@users.noreply.github.com>
2024-02-20 21:40:38 -0800

    Create About.js

---
+++ src/About.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+export default function About() {
+  return(
+    <div className="contentPage">
+      <h1> About </h1>
+      <p> 
+        Serving happiness through incredible food since 1673, McMaani's has been what can be considered as a phenomenon.
+        People rush to get themselves the iconic "Mega Burger", as well as the Fries and Chicken Nuggets. Our tasty
+        food comes from our secret formulas, that derived from ancient scrolls written on cave walls. 
+      </p>
+      <p>
+        So even cavemen were enjoying McMaani's burgers!
+      </p>
+      <p>
+        Order your food now to have it delivered with the fastest delivery in the fast food market! Or, come in
+        at your local McMaani's and enjoy a taste of true happiness! We will always be here to serve you, and ensure
+        that even the bad days are turned upside down!
+      </p>      
+    </div>
+  )
+}
